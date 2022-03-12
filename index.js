3f5f0554c22a93ce6106b10ff0fa9ef2b575e7a5
Vaibhav Singh <88846265+065vaibhav@users.noreply.github.com>
2022-03-12 23:29:34 +0000

    made new function for geting every nth index of array

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+///////////////////Sort of array//////////////////////////////////
 function removeDuplicate(arr){
   return arr.filter((newA,index)=>arr.indexOf(newA) === index);
 }
@@ -6,4 +7,20 @@ function myFunction(arr){
   return removeDuplicate(arr).sort((a,b)=>a-b);
 }
 
-console.log(myFunction([55,6,41,96,21,83,21]))
+myFunction([55,6,41,96,21,83,21])
+
+
+/////////////////Get every possible value of nth index from array////////////////
+
+function myFunction(a,n){
+  var count = n-1
+  var newval='';
+  var newArr = []
+  for (let index = 0; index < Math.floor(a.length/n); index++) {
+    newval =  a[count]
+      newArr.push(newval)
+    count+=n
+  }
+  return newArr;
+
+myFunction([7,2,1,6,3,4,5,8,9,10,11,12,13],5)
