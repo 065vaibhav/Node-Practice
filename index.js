ba267563d4c0bab71f9da79152616d2e2e5c79a3
Vaibhav Singh <88846265+065vaibhav@users.noreply.github.com>
2022-03-21 11:18:20 +0000

    updates changes in text

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-///////////Multiply all object values by x///////////////////////////
-function myFunction(a,b){
-  var values = Object.fromEntries(
-    Object.entries(a).map(([key,value])=> [ key, value*b])
-  );
-  
-  return values;
+///////////find the largest string from array///////////////////////////
+function myFunction(a){
+  var valu =a.sort(function(a,b){
+    return b.length-a.length;
+  })
+  return valu[0]
 }
 
-myFunction({a:1,b:2,c:3},3)
+myFunction(['Irrrrrrrrrrrrrrrrr', 'needyyy', 'candy'])
 
 ///////////////////Sort of array//////////////////////////////////
-function removeDuplicate(arr){
-  return arr.filter((newA,index)=>arr.indexOf(newA) === index);
-}
+// function removeDuplicate(arr){
+//   return arr.filter((newA,index)=>arr.indexOf(newA) === index);
+// }
 
-function myFunction(arr){
-  return removeDuplicate(arr).sort((a,b)=>a-b);
-}
+// function myFunction(arr){
+//   return removeDuplicate(arr).sort((a,b)=>a-b);
+// }
 
-myFunction([55,6,41,96,21,83,21])
+// myFunction([55,6,41,96,21,83,21])
 
 
 /////////////////Get every possible value of nth index from array////////////////
 
-function myFunction(a,n){
-  var count = n-1
-  var newval='';
-  var newArr = []
-  for (let index = 0; index < Math.floor(a.length/n); index++) {
-    newval =  a[count]
-      newArr.push(newval)
-    count+=n
-  }
-  return newArr;
+// function myFunction(a,n){
+//   var count = n-1
+//   var newval='';
+//   var newArr = []
+//   for (let index = 0; index < Math.floor(a.length/n); index++) {
+//     newval =  a[count]
+//       newArr.push(newval)
+//     count+=n
+//   }
+//   return newArr;
 
-myFunction([7,2,1,6,3,4,5,8,9,10,11,12,13],5)
+// myFunction([7,2,1,6,3,4,5,8,9,10,11,12,13],5)
 
