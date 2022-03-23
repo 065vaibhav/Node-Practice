0a84cf38657120225356587dc9fd3897c903c3bd
Vaibhav Singh <88846265+065vaibhav@users.noreply.github.com>
2022-03-23 08:06:24 +0000

    update new changes

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-///////////find the largest string from array///////////////////////////
-function myFunction(a){
-  var valu =a.sort(function(a,b){
-    return b.length-a.length;
-  })
-  return valu[0]
+///////////////////Insert character after every n characters (backwards)/////
+myFunction=(a,b)=>{
+   let result = [];
+   let rest = a;
+   while (rest.length) {
+   	result.push(rest.slice(-3));
+   	rest = rest.slice(0, -3);
+   }
+  console.log(result.reverse().join(b))
 }
 
-myFunction(['Irrrrrrrrrrrrrrrrr', 'needyyy', 'candy'])
+myFunction('1234567','.')
+
+///////////////////increment every character at string/////////////
+// myFunction = (a) =>{
+//   var x=""
+//   for (let i = 0; i < a.length; i++) {
+//     x += String.fromCharCode(a.split("")[i].charCodeAt() + 1)
+//   }
+//   return x
+// }
+// console.log(myFunction('bgddrd'))
+
+///////////find the largest string from array///////////////////////////
+// function myFunction(a){
+//   var valu =a.sort(function(a,b){
+//     return b.length-a.length;
+//   })
+//   return valu[0]
+// }
+
+// myFunction(['Irrrrrrrrrrrrrrrrr', 'needyyy', 'candy'])
 
 ///////////////////Sort of array//////////////////////////////////
 // function removeDuplicate(arr){
@@ -31,7 +54,7 @@ myFunction(['Irrrrrrrrrrrrrrrrr', 'needyyy', 'candy'])
 //       newArr.push(newval)
 //     count+=n
 //   }
-//   return newArr;
-
+//   console.log(newArr) ;
+// }
 // myFunction([7,2,1,6,3,4,5,8,9,10,11,12,13],5)
 
